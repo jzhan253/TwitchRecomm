bb96ed8a6dcd8981e349d1b74de21bc934f6ed40
Jinfan <kimthebestman@gmail.com>
2021-06-01 20:28:54 -0700

    d3

---
+++ src/main/java/com/jincreation/jupiter/external/TwitchException.java
@@ -0,0 +1,7 @@
+package com.jincreation.jupiter.external;
+
+public class TwitchException extends RuntimeException {
+    public TwitchException(String errorMessage) {
+        super(errorMessage);
+    }
+}
--- src/main/java/com/jincreation/jupiter/servlet/GameServlet.java
+++ src/main/java/com/jincreation/jupiter/servlet/GameServlet.java
@@ -2,6 +2,8 @@ package com.jincreation.jupiter.servlet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jincreation.jupiter.entity.Game;
+import com.jincreation.jupiter.external.TwitchClient;
+import com.jincreation.jupiter.external.TwitchException;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
 
@@ -15,38 +17,27 @@ import java.io.IOException;
 @WebServlet(name = "GameServlet", urlPatterns = {"/game"})
 public class GameServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        JSONObject jsonRequest = new JSONObject(IOUtils.toString(request.getReader()));
-        String name = jsonRequest.getString("name");
-        String developer = jsonRequest.getString("developer");
-        String releaseTime = jsonRequest.getString("release_time");
-        String website = jsonRequest.getString("website");
-        float price = jsonRequest.getFloat("price");
-        // Print game information to IDE console
-        System.out.println("Name is: " + name);
-        System.out.println("Developer is: " + developer);
-        System.out.println("Release time is: " + releaseTime);
-        System.out.println("Website is: " + website);
-        System.out.println("Price is: " + price);
-        // Return status = ok as response body to the client
-        response.setContentType("application/json");
-        JSONObject jsonResponse = new JSONObject();
-        jsonResponse.put("status", "ok");
-        response.getWriter().print(jsonResponse);
+        
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("application/json");
-        ObjectMapper mapper = new ObjectMapper();
-        Game.Builder builder = new Game.Builder();
-        builder.setName("World of Warcraft");
-        builder.setDeveloper("Blizzard Entertainment");
-        builder.setReleaseTime("Feb 11, 2005");
-        builder.setWebsite("https://www.worldofwarcraft.com");
-        builder.setPrice(49.99);
+        // Get gameName from request URL.
+        String gameName = request.getParameter("game_name");
+        TwitchClient client = new TwitchClient();
 
+        // Let the client know the returned data is in JSON format.
+        response.setContentType("application/json;charset=UTF-8");
+        try {
+            // Return the dedicated game information if gameName is provided in the request URL, otherwise return the top x games.
+            if (gameName != null) {
+                response.getWriter().print(new ObjectMapper().writeValueAsString(client.searchGame(gameName)));
+            } else {
+                response.getWriter().print(new ObjectMapper().writeValueAsString(client.topGames(0)));
+            }
+        } catch (TwitchException e) {
+            throw new ServletException(e);
+        }
 
-        Game game = builder.build();
-        response.getWriter().print(mapper.writeValueAsString(game));
     }
 }
