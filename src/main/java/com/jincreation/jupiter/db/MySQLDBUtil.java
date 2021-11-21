1a646bebf15c2906c32dc5219dc34be9fb8ce25b
Jinfan Zhang <62904466+jzhan253@users.noreply.github.com>
2021-11-20 17:09:08 -0800

    Update MySQLDBUtil.java

--- src/main/java/com/jincreation/jupiter/db/MySQLDBUtil.java
+++ src/main/java/com/jincreation/jupiter/db/MySQLDBUtil.java
@@ -5,7 +5,7 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class MySQLDBUtil {
-    private static final String INSTANCE = "projects.cing882ce6yu.us-west-1.rds.amazonaws.com";
+    private static final String INSTANCE = 
     private static final String PORT_NUM = "3306";
     private static final String DB_NAME = "twitch_db";
     public static String getMySQLAddress() throws IOException {
